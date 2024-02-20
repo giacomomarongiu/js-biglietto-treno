5b12b9789d04bce543b10b39af6c8faf425437a0
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-20 15:08:38 +0100

    init repo - first structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-/*console.log("Ciao, il tuo js è ben collegato");
+console.log("Ciao, il tuo js è ben collegato");
 
 
-/*- Dichiaro due variabili "age" e "km" e specifico che mi serve un valore numerico
-- Assegno loro un valore tramite promt
+//- Dichiaro due variabili "age" e "km" e specifico che mi serve un valore numerico
+//  - Assegno loro un valore tramite promt
 
 
-let user_age = Number(prompt("Inserisci la tua età"));
+let user_age = Number(document.getElementById("my_user_age"));
 
-let user_km = Number(prompt("Inserisci la quantità di chilometri che desideri percorrere"));
+let user_km = Number(document.getElementById("my_user_km"));
 
 console.log("Bene, hai inserito i seguenti valori", user_age, user_km);
 
@@ -30,76 +30,8 @@ console.log("Il prezzo pieno per il tuo biglietto è di ", formatter.format(tota
 //- Confronto il valore della variabile contentente "age" con 18
 //- SE eta è SUPERIORE o UGUALE
 if (user_age >= 18) {
-    /*- - Confronto il valore della variabile contentente "age" con 65
+    //- - Confronto il valore della variabile contentente "age" con 65
     // - - SE "age" è superiore applico a "total" uno sconto del 40% e lo mostro
-    if (user_age >= 65) {
-        total =(total) - (total * 40 / 100) //Over 65 price
-        console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
-        // - - SE NO Visualizzo "total" senza sconti
-    } else {
-        total = total //Full price
-        console.log("Peccato, non ti spetta nessuno sconto, pagherai", formatter.format(total));
-    }
-} else {
-//- SE NO 
-    total = (total) - (total * 20 / 100) // Under 18 price
-    console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
-}
-
-*/
-
-
-
-// STESSO ESERCIZIO MA INSERENDO L'ANNO DI NASCITA
-
-console.log("Ciao, il tuo js è ben collegato");
-
-
-/*- Dichiaro due variabili "age" e "km" e specifico che mi serve un valore numerico
-- Assegno loro un valore tramite promt*/
-
-
-let user_age = Number(prompt("Inserisci il tuo anno di nascita"));
-
-let user_km = Number(prompt("Inserisci la quantità di chilometri che desideri percorrere"));
-
-console.log("Bene, hai inserito i seguenti valori", user_age, user_km);
-
-//console.log(typeof user_age, typeof user_km); Verify number type
-
-
-// - Calcolo quanto sarà la spesa totale sulla base di
-//questi dati e lo assegno a una nuova variabile "total"
-
-let total = (user_km * 0.21)
-
-//- Specifico la formattazione con cui voglio visualizzare i numeri
-const formatter = new Intl.NumberFormat('it-IT', {
-    style: 'currency',
-    currency: 'EUR',
-})
-
-console.log("Il prezzo pieno per il tuo biglietto è di ", formatter.format(total));
-
-//Inserisco in una variabile la data di oggi
-const my_date = new Date();
-console.log("Bene, oggi è il ", my_date);
-
-//Dichiaro una variabile per salvarci dentro l'anno corrente
-let today_year = my_date.getFullYear(my_date);
-//console.log(today_year); 
-
-//Calcolo l'età sottraendo l'anno di nascita dall'anno di oggi
-user_age = (today_year - user_age)
-
-console.log("Bene, hai (o stai per compiere)", user_age, " anni, vediamo se c'è qualche sconto per te");
-
-
-//- Confronto il valore della variabile contentente "age" con 18
-//- SE eta è SUPERIORE o UGUALE
-if (user_age > 18) {
-    /*- - Confronto il valore della variabile contentente "age" con 65
-    // - - SE "age" è superiore applico a "total" uno sconto del 40% e lo mostro*/
     if (user_age >= 65) {
         total = (total) - (total * 40 / 100) //Over 65 price
         console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
@@ -108,27 +40,12 @@ if (user_age > 18) {
         total = total //Full price
         console.log("Peccato, non ti spetta nessuno sconto, pagherai", formatter.format(total));
     }
-}
-
-// VERIFICO SE HA GIA' COMPIUTO 18 ANNI
-else if (user_age == 18) {
-    //Dichiaro una variabile per salvarci dentro il mese corrente
-    let user_month = Number(prompt("Inserisci il numero corrispondete al tuo mese di nascita"));
-    // SE IL MIO MESE DI NASCITA E' SUPERIORE NON LI HO COMPIUTI
-    if (user_month > my_date.getMonth(my_date)) {
-        total = (total) - (total * 20 / 100) // Under 18 price
-        console.log("Non hai ancora 18 anni!")
-        console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));    
-    }
-    //SE E' INFERIORE SI', NESSUNO SCONTO
-    else {
-        total = total //Full price
-        console.log("Peccato, non ti spetta nessuno sconto, pagherai", formatter.format(total));
-    }
-}
-else {
+} else {
     //- SE NO 
     total = (total) - (total * 20 / 100) // Under 18 price
     console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
 }
+
+
+
 
--- readme.md
+++ readme.md
@@ -1,17 +1,23 @@
 # Instruction
 Descrizione:
-Trenitalia ha bisogno di un programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+Scrivere un programma che chieda all’utente:
+Il numero di chilometri da percorrere
+Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65 (fortunelli :older_adult:).
-L'output del prezzo finale va messo fuori in forma umana (con massimo due cifre decimali, per indicare centesimi sul prezzo).
-Questo richiederà un minimo di ricerca - come quelle fatte in classe :wink:
+va applicato uno sconto del 40% per gli over 65.
+
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+Nota: Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
 
 
 ## Steps
 - Dichiaro due variabili "eta" e "km" e specifico che mi serve un valore numerico
-- Assegno loro un valore tramite promt
+- Assegno loro un valore tramite l'input dell'html
 - Calcolo quanto sarà la spesa totale sulla base di questi dati e lo assegno a una nuova variabile "total"
 - Specifico la formattazione con cui voglio visualizzare i numeri
 - Confronto il valore della variabile contentente "eta" con 18
@@ -25,8 +31,7 @@ Questo richiederà un minimo di ricerca - come quelle fatte in classe :wink:
 
 ### Tools
 variabili
-funzioni
-promnt
+querySelector
 Intl.NumberFormat object
 if else
 log
