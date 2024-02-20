ded6da41d8a1c0512aa4faa8fcfe532c21cd04ad
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-20 16:10:48 +0100

    milestone 1 completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,63 +5,45 @@ console.log("Ciao, il tuo js è ben collegato");
 //  - Assegno loro un valore tramite promt
 
 
-let user_age = 0;
+let user_age;
 
-let user_km = 0;
+let user_km;
 
 //- Creo una funzione dentro add.EventListener
 
-document.querySelector('.btn_age').addEventListener('click', function(){
-    let user_age = Number(document.getElementById("my_user_age").value);
-})
+document.querySelector('.btn_element').addEventListener('click', function(){
+    user_age = Number(document.getElementById("my_user_age").value);
+    user_km = Number(document.getElementById("my_user_km").value);
+    console.log("Bene, hai inserito i seguenti valori", user_age, user_km);
+    let total = (user_km * 0.21)
 
-document.querySelector('.btn_km').addEventListener('click', function(){
-    let user_km = Number(document.getElementById("my_user_km").value);
-})
-
-
-console.log("Bene, hai inserito i seguenti valori", user_age, user_km);
-
-//console.log(typeof user_age, typeof user_km); Verify number type
-
-
-
-
-
-
-
-// - Calcolo quanto sarà la spesa totale sulla base di
-//questi dati e lo assegno a una nuova variabile "total"
-
-let total = (user_km * 0.21)
-
-//- Specifico la formattazione con cui voglio visualizzare i numeri
-const formatter = new Intl.NumberFormat('it-IT', {
-    style: 'currency',
-    currency: 'EUR',
-})
-
-console.log("Il prezzo pieno per il tuo biglietto è di ", formatter.format(total));
-
-//- Confronto il valore della variabile contentente "age" con 18
-//- SE eta è SUPERIORE o UGUALE
-if (user_age >= 18) {
-    //- - Confronto il valore della variabile contentente "age" con 65
-    // - - SE "age" è superiore applico a "total" uno sconto del 40% e lo mostro
-    if (user_age >= 65) {
-        total = (total) - (total * 40 / 100) //Over 65 price
-        console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
-        // - - SE NO Visualizzo "total" senza sconti
+    //- Specifico la formattazione con cui voglio visualizzare i numeri
+    const formatter = new Intl.NumberFormat('it-IT', {
+        style: 'currency',
+        currency: 'EUR',
+    })
+    
+    console.log("Il prezzo pieno per il tuo biglietto è di ", formatter.format(total));
+    
+    //- Confronto il valore della variabile contentente "age" con 18
+    //- SE eta è SUPERIORE o UGUALE
+    if (user_age >= 18) {
+        //- - Confronto il valore della variabile contentente "age" con 65
+        // - - SE "age" è superiore applico a "total" uno sconto del 40% e lo mostro
+        if (user_age >= 65) {
+            total = (total) - (total * 40 / 100) //Over 65 price
+            console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
+            // - - SE NO Visualizzo "total" senza sconti
+        } else {
+            total = total //Full price
+            console.log("Peccato, non ti spetta nessuno sconto, pagherai", formatter.format(total));
+        }
     } else {
-        total = total //Full price
-        console.log("Peccato, non ti spetta nessuno sconto, pagherai", formatter.format(total));
+        //- SE NO 
+        total = (total) - (total * 20 / 100) // Under 18 price
+        console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
     }
-} else {
-    //- SE NO 
-    total = (total) - (total * 20 / 100) // Under 18 price
-    console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
-}
 
-
+})
 
 
