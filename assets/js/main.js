94cd68354bcbf35843cd4785abe565e40db9453b
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-20 16:45:58 +0100

    used querySelector and innerHtml to change my html

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ let user_km;
 
 //- Creo una funzione dentro add.EventListener
 
-document.querySelector('.btn_element').addEventListener('click', function(){
+document.querySelector('.btn_element').addEventListener('click', function () {
     user_age = Number(document.getElementById("my_user_age").value);
     user_km = Number(document.getElementById("my_user_km").value);
     console.log("Bene, hai inserito i seguenti valori", user_age, user_km);
@@ -22,9 +22,9 @@ document.querySelector('.btn_element').addEventListener('click', function(){
         style: 'currency',
         currency: 'EUR',
     })
-    
+
     console.log("Il prezzo pieno per il tuo biglietto è di ", formatter.format(total));
-    
+
     //- Confronto il valore della variabile contentente "age" con 18
     //- SE eta è SUPERIORE o UGUALE
     if (user_age >= 18) {
@@ -33,17 +33,46 @@ document.querySelector('.btn_element').addEventListener('click', function(){
         if (user_age >= 65) {
             total = (total) - (total * 40 / 100) //Over 65 price
             console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
+
+
+            let recapElement = document.querySelector('.recap')
+
+            recapElement.innerHTML += user_age + "anni e " + user_km + "chilometri"
+
+            let ticketElement = document.querySelector('.ticket')
+
+            ticketElement.innerHTML += total
+
             // - - SE NO Visualizzo "total" senza sconti
         } else {
             total = total //Full price
             console.log("Peccato, non ti spetta nessuno sconto, pagherai", formatter.format(total));
+
+
+            let recapElement = document.querySelector('.recap')
+
+            recapElement.innerHTML += user_age + "anni e " + user_km + "chilometri"
+
+            let ticketElement = document.querySelector('.ticket')
+
+            ticketElement.innerHTML += total
+
         }
     } else {
         //- SE NO 
         total = (total) - (total * 20 / 100) // Under 18 price
         console.log("Il prezzo scontato per il tuo biglietto è di ", formatter.format(total));
+
+
+        let recapElement = document.querySelector('.recap')
+
+        recapElement.innerHTML += user_age + "anni e " + user_km + "chilometri"
+
+        let ticketElement = document.querySelector('.ticket')
+
+        ticketElement.innerHTML += total
+
     }
 
 })
-
 
